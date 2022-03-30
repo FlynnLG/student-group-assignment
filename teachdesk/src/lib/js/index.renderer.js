d3031be19a016f4497361cdde62cd5849adbcf69
VD-lab <58665605+OctoDino@users.noreply.github.com>
2022-03-30 19:04:03 +0200

    Sweet Source Code :)

---
+++ teachdesk/src/lib/js/index.renderer.js
@@ -0,0 +1,17 @@
+document.querySelector('#add').addEventListener('click', function(e) {
+    window.location.href = './addThings.html';
+}, false);
+
+
+
+document.querySelector('.collum1').addEventListener('click', function(e) {
+    window.location.href = './ask.random.html';
+}, false);
+
+document.querySelector('.collum2').addEventListener('click', function(e) {
+    window.location.href = './ask.grouper.html';
+}, false);
+
+document.querySelector('.collum3').addEventListener('click', function(e) {
+    window.location.href = './classmanager.html';
+}, false);
