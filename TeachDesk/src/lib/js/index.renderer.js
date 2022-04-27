cca346ea28903c6145eb922f9daaa0947ad92be8
VD-lab <58665605+OctoDino@users.noreply.github.com>
2022-04-27 14:54:51 +0200

    some changes

--- TeachDesk/src/lib/js/index.renderer.js
+++ TeachDesk/src/lib/js/index.renderer.js
@@ -1,5 +1,3 @@
-import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
-
 document.querySelector('.collum1').addEventListener('click', function(e) {
     window.location.href = './ask.random.html';
 }, false);
@@ -8,18 +6,19 @@ document.querySelector('.collum2').addEventListener('click', function(e) {
     window.location.href = './ask.grouper.html';
 }, false);
 
+document.querySelector('.collum3').addEventListener('click', function(e) {
+    window.location.href = '../notes/index.html';
+}, false);
+
 //Get from GitHub the Relases form the Repo and console.log those
 //https://api.github.com/repos/electron/electron/releases
 //https://api.github.com/repos/electron/electron/releases/latest
 currentVersion = "1.0.0-beta.2";
 
-response = await octokit.request('GET /repos/{owner}/{repo}/releases', {
-    owner: 'flynnlg',
-    repo: 'student-group-assignment'
-  })
 
-  console.log(response);
 
   //check response for name
     //https://api.github.com/repos/flynnlg/student-group-assignment/releases/latest
+
+    //If update is aviable, show update message
 
